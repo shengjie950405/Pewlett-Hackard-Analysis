1f97ee33a0da31677734d8d40bf7658695dd6763
shengjie950405 <111814578+shengjie950405@users.noreply.github.com>
2022-10-12 21:03:31 -0400

    Add files via upload

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,53 @@
+-- Deliverable 1: The Number of Retiring Employees by Title 
+
+-- Create new retirement_titles table
+
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM employees as e
+       INNER JOIN titles AS t
+	           ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no ASC;
+-- Remove Duplicate rows
+
+Select Distinct ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE rt.to_date = '9999-01-01'
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+select * from unique_titles
+
+-- Retrieve the number of employees by their most recent job title who are about to retire.
+
+SELECT COUNT(ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+
+Select * from retiring_titles
+
+-- Deliverable 2 
+
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+			INNER JOIN titles AS t
+				ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
+
+select * from mentorship_eligibility;
